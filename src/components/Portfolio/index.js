c64f18ef88ab9f64ef6aeda3b63f7eb592269299
William Berry <76190311+wberry86@users.noreply.github.com>
2021-07-23 15:13:26 -0500

    added final project to portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -5,6 +5,7 @@ import robotImage from "../../assets/project/images/robot.PNG";
 import dogMapsImage from "../../assets/project/images/dog-maps-screenshot.PNG";
 import readmeImage from "../../assets/project/images/readme-capture.PNG";
 import employeeImage from "../../assets/project/images/employee.png";
+import socialNetworker from "../../assets/project/images/social.PNG";
 import { Link } from 'react-router-dom';
 
 class Portfolio extends Component {
@@ -22,7 +23,10 @@ class Portfolio extends Component {
         alt="project 1"
       />
       </Link>
-
+      <Link to={{ pathname: "https://github.com/wberry86/code-generator/" }} target="_blank">Link to GitHub page</Link>
+      <br/>
+      <br/>
+      <br/>
       <Link to={{ pathname: "https://wberry86.github.io/work-day-scheduler/" }} target="_blank">
         <h2>Code-Refactor</h2>
         <img
@@ -32,7 +36,10 @@ class Portfolio extends Component {
         alt="project 2"
       />
       </Link>
-
+      <Link to={{ pathname: "https://github.com/wberry86/work-day-scheduler" }} target="_blank">Link to GitHub page</Link>
+      <br/>
+      <br/>
+      <br/>
       <Link to={{ pathname: "https://wberry86.github.io/robot-gladiators/" }} target="_blank">
         <h2>Robot-Gladiators</h2>
         <img
@@ -42,7 +49,10 @@ class Portfolio extends Component {
         alt="project 3"
       />
       </Link>
-
+      <Link to={{ pathname: "https://github.com/wberry86/robot-gladiators" }} target="_blank">Link to GitHub page</Link>
+      <br/>
+      <br/>
+      <br/>
       <Link to={{ pathname: "https://wberry86.github.io/dog-maps/" }} target="_blank">
         <h2>Dog-Maps Group Project</h2>
         <img
@@ -52,7 +62,10 @@ class Portfolio extends Component {
         alt="project 4"
       />
       </Link>
-
+      <Link to={{ pathname: "https://github.com/wberry86/dog-maps" }} target="_blank">Link to GitHub page</Link>
+      <br/>
+      <br/>
+      <br/>
       <Link to={{ pathname: "https://github.com/wberry86/professional-README-generator" }} target="_blank">
         <h2>Professional-README-Generator</h2>
         <img
@@ -62,7 +75,9 @@ class Portfolio extends Component {
         alt="project 5"
       />
       </Link>
-
+      <br/>
+      <br/>
+      <br/>
       <Link to={{ pathname: "https://github.com/wberry86/employee-tracker" }} target="_blank">
         <h2>Employee-Manager</h2>
         <img
@@ -72,6 +87,19 @@ class Portfolio extends Component {
         alt="project 6"
       />
       </Link>
+      <br/>
+      <br/>
+      <br/>
+      <Link to={{ pathname: "https://social-networker.herokuapp.com/" }} target="_blank">
+        <h2>Social-Networker</h2>
+        <img
+        src={socialNetworker}
+        className="my-2"
+        style={{ width: "100%" }}
+        alt="project 7"
+      />
+      </Link>
+      <Link to={{ pathname: "https://github.com/JosephSuvak/The-Social-Network" }} target="_blank">Link to GitHub page</Link>
     </div>
     </div>
   );
